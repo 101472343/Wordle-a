55e11fab2f5bf7e67d5ec200f91c03a00777c0fe
tuck295q <101472343@georgebrown.ca>
2023-12-15 18:27:19 -0500

    Fix line endings in .gitignore

---
+++ README.txt
@@ -0,0 +1,30 @@
+Instructions:
+
+1) Open Powershell Terminal
+
+2) Create directory and change directory by typing as follows:
+- to make directory type: mkdir "Folder Name"
+- to change to that directory, type: cd "Folder Name" 
+
+3) Clone the Repository -
+Open your terminal or command prompt and run the following command to clone the Wordle-anche! repository:
+
+git clone https://github.com/sabazubair/wordle-anche.git
+
+4) Navigate to the Project Directory - 
+Change your working directory to the newly cloned Wordle-anche! folder:
+
+cd wordle-anche
+
+5) Install Dependencies -
+Run the following command to install the necessary dependencies:
+
+npm install
+
+6) Run the Application - 
+Once the installation is complete, start the Wordle-anche! application:
+
+npm start
+
+This command will start the application, and you should be able to access it by opening your web browser and navigating to http://localhost:3000/ (or another port if specified).
+
---
+++ src/App.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"; // Import this for better matchers
+import App from "./App"; // Import your component
+
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".row"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".title"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".keyboard-wrapper"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".game-tile"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".letter"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
